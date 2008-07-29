697001c95fed18da17d466645d0552da93ffc8d1
Stefano Costa <steko@iosa.it>
2008-07-29 22:09:31 +0200

    tops_csv.py: some minor fixes

--- output/csv/tops_csv.py
+++ output/csv/tops_csv.py
@@ -13,12 +13,20 @@ class TotalOpenCSV:
     
     ``data`` should be an iterable (e.g. list) containing one iterable (e.g.
     tuple) for each point. The default order is PID, x, x, z, TEXT.
+    
+    This is consistent with our current standard.
     """
     
     def __init__(self,data,filepath):
-        writer = csv.writer(open(filepath, "wb"), quoting=QUOTE_NONNUMERIC)
+        writer = csv.writer(open(filepath, "wb"), quoting=csv.QUOTE_NONNUMERIC)
         writer.writerow(('PID', 'x', 'y', 'z', 'TEXT'))
         writer.writerows(data)
 
 if __name__ == "__main__":
-    TotalOpenCSV([(1,2,3,4,'primo punto'), (5,6,7,8,'secondo punto')], 'p.csv')
+    TotalOpenCSV(
+        [
+            (1,2,3,4,'qwerty'),
+            ("2.3",42,45,12,'asdfg')
+        ],
+    'p.csv')
+
