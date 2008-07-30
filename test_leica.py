d001c1628998ff540cdbb60401a0e486fc492de5
Stefano Costa <steko@iosa.it>
2008-07-30 22:51:17 +0200

    test_leica.py: add support for CSV output format.
    The desidered output format can be specified on the command line
    as argument after the module name.

--- test_leica.py
+++ test_leica.py
@@ -4,12 +4,15 @@
 # Copyright 2008 Stefano Costa <steko@iosa.it>
 # Under the GNU GPL 3 License
 
+import sys
+
 from output.dxf.sdxf import *
-from models import tops
+from output.csv.tops_csv import TotalOpenCSV
+from models import leica_tcr_1205
 
 # read TS data
 
-main = tops.LeicaTCR1205('/home/gqb/code/totalopenstation/models/leica_1205_1.txt')
+main = leica_tcr_1205.LeicaTCR1205('/home/gqb/code/totalopenstation/models/leica_1205_1.txt')
 main.parse_retrieve_data()
 punti = main.points.list_to_tuple()
 
@@ -18,26 +21,28 @@ codici = set([ p[4] for p in punti ])
 
 # create DXF
 
-#Drawing
-d=Drawing(layers=())
-#tables
-d.styles.append(Style())                #table styles
-#d.styles.append(Style( name='GQB', height=0.04 ))
-d.views.append(View('Normal'))          #table view
-d.views.append(ViewByWindow('Window',leftBottom=(1,0),rightTop=(2,1)))  #idem
-
-for n, i in enumerate(codici):
-    name_p = "%s_PUNTI" % i
-    name_q = "%s_QUOTE" % i
-    name_n = "%s_NUMERI" % i
-    d.append(Layer(name=name_p, color=n))
-    d.append(Layer(name=name_q, color=n))
-    d.append(Layer(name=name_n, color=n))
-
-for p in punti:
-    p_id, p_x, p_y, p_z, p_layer = p
+def make_dxf():
+    """Creates a DXF drawing with the available dataset."""
     
-    if p_layer < 1200:
+    #Drawing
+    d=Drawing(layers=())
+    #tables
+    d.styles.append(Style())                #table styles
+    #d.styles.append(Style( name='GQB', height=0.04 ))
+    d.views.append(View('Normal'))          #table view
+    d.views.append(ViewByWindow('Window',leftBottom=(1,0),rightTop=(2,1)))  #idem
+
+    for n, i in enumerate(codici):
+        name_p = "%s_PUNTI" % i
+        name_q = "%s_QUOTE" % i
+        name_n = "%s_NUMERI" % i
+        d.append(Layer(name=name_p, color=n))
+        d.append(Layer(name=name_q, color=n))
+        d.append(Layer(name=name_n, color=n))
+
+    for p in punti:
+        p_id, p_x, p_y, p_z, p_layer = p
+        
         name_p = "%s_PUNTI" % p_layer
         name_q = "%s_QUOTE" % p_layer
         name_n = "%s_NUMERI" % p_layer
@@ -51,5 +56,18 @@ for p in punti:
         # add Z value
         d.append(Text(str(p_z), point=(p_x, p_y, 0), layer=name_q ))
 
-d.saveas('leica.dxf')
+    d.saveas('leica.dxf')
+
+def make_csv():
+    csv_output = TotalOpenCSV(punti,'leica.csv')
+
+if len(sys.argv) > 1:
+#    if sys.argv[1] == 'csv':
+#        make_csv()
+#    if sys.argv[1] == 'dxf':
+#        make_dxf()
+    try:
+        eval("make_%s()" % sys.argv[1])
+    except NameError:
+        print "Output format %s is not recognized" % sys.argv[1]
 
