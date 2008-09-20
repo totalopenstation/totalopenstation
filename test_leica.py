ca0ddc811bb097ccd3da179d3e71d8d6215f1d2a
Stefano Costa <steko@iosa.it>
2008-09-20 16:49:48 +0200

    adjust wrong merge.

--- test_leica.py
+++ test_leica.py
@@ -4,40 +4,45 @@
 # Copyright 2008 Stefano Costa <steko@iosa.it>
 # Under the GNU GPL 3 License
 
+import sys
+
 from output.dxf.sdxf import *
+from output.csv.tops_csv import TotalOpenCSV
 from models import leica_tcr_1205
 
 # read TS data
 
-main = leica_tcr_1205.LeicaTCR1205('models/leica_1205_1.txt')
-punti = main.t_points
-
+main = leica_tcr_1205.LeicaTCR1205('/home/gqb/code/totalopenstation/models/leica_1205_1.txt')
+main.parse_retrieve_data()
+punti = main.points.list_to_tuple()
 
 
 codici = set([ p[4] for p in punti ])
 
 # create DXF
 
-#Drawing
-d=Drawing(layers=())
-#tables
-d.styles.append(Style())                #table styles
-#d.styles.append(Style( name='GQB', height=0.04 ))
-d.views.append(View('Normal'))          #table view
-d.views.append(ViewByWindow('Window',leftBottom=(1,0),rightTop=(2,1)))  #idem
-
-for n, i in enumerate(codici):
-    name_p = "%s_PUNTI" % i
-    name_q = "%s_QUOTE" % i
-    name_n = "%s_NUMERI" % i
-    d.append(Layer(name=name_p, color=n))
-    d.append(Layer(name=name_q, color=n))
-    d.append(Layer(name=name_n, color=n))
-
-for p in punti:
-    p_id, p_x, p_y, p_z, p_layer = p
+def make_dxf():
+    """Creates a DXF drawing with the available dataset."""
     
-    if p_layer < 1200:
+    #Drawing
+    d=Drawing(layers=())
+    #tables
+    d.styles.append(Style())                #table styles
+    #d.styles.append(Style( name='GQB', height=0.04 ))
+    d.views.append(View('Normal'))          #table view
+    d.views.append(ViewByWindow('Window',leftBottom=(1,0),rightTop=(2,1)))  #idem
+
+    for n, i in enumerate(codici):
+        name_p = "%s_PUNTI" % i
+        name_q = "%s_QUOTE" % i
+        name_n = "%s_NUMERI" % i
+        d.append(Layer(name=name_p, color=n))
+        d.append(Layer(name=name_q, color=n))
+        d.append(Layer(name=name_n, color=n))
+
+    for p in punti:
+        p_id, p_x, p_y, p_z, p_layer = p
+        
         name_p = "%s_PUNTI" % p_layer
         name_q = "%s_QUOTE" % p_layer
         name_n = "%s_NUMERI" % p_layer
@@ -51,5 +56,16 @@ for p in punti:
         # add Z value
         d.append(Text(str(p_z), point=(p_x, p_y, 0), layer=name_q ))
 
-d.saveas('leica.dxf')
+    d.saveas('leica.dxf')
+
+def make_csv():
+    csv_output = TotalOpenCSV(punti,'leica.csv')
+
+if len(sys.argv) > 1:
+    try:
+        eval("make_%s()" % sys.argv[1])
+    except NameError:
+        print "Output format %s is not recognized" % sys.argv[1]
+else:
+    print punti[0:30]
 
