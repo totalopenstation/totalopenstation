77c432b5e44f51aac25a2b91014f3ae1d0847f16
Stefano Costa <steko@iosa.it>
2008-09-24 22:07:07 +0200

    fix wrong line slice for Zeiss parser.
    BTW, why the hell did I have tabs instead of spaces??

--- models/zeiss_elta_r55.py
+++ models/zeiss_elta_r55.py
@@ -8,72 +8,73 @@ class ZeissConn(Connector):
     pass
 
 class ZeissEltaR55(TotalStation):
-	
-	def __init__(self, filename):
-		TotalStation.__init__(self, filename, swapXY=True)
-	
-	def is_point(self,line):
+    
+    def __init__(self, filename):
+        TotalStation.__init__(self, filename, swapXY=True)
+    
+    def is_point(self,line):
 
-		tokens = {
-			'sequence' : line[0:7],
-			'pid' : line[8:27],
-			'text' : line[27:32],
-			'X_str' : line[36],
-			'x' : line[38:50],
-			'Y_str' : line[51],
-			'y' : line[53:66],
-			'Z_str' : line[67],
-			'z' : line[69:80]
-			}
-		
-		try:
-			int(tokens['sequence'])
-			int(tokens['pid'])
-			float(tokens['x'])
-			float(tokens['y'])
-			float(tokens['z'])
-		
-		except (ValueError, IndexError):
-			
-			is_point = False
-		
-		else:
-			
-			is_point = True
-		
-		return is_point
-	
-	def get_point(self,line):
-		
-		text = line[8:27].split()
-		
-		if len(text) >= 1 :
-			aux = text[0]
-		else:
-			aux = ""
-		
-		tokens = {
-			'pid' : line[8:27].split()[0],   # the result is more elegant than
-			'text' : aux, # the code (Heisenberg rocks!)
-			'x' : line[38:50].split()[0],
-			'y' : line[53:66].split()[0],
-			'z' : line[69:80].split()[0]
-			}
-					
-		point_id = int(tokens['pid'])
-		text = str(tokens['text'])
-		
-		# note that for now we keep floats into strings to avoid approximation
-		# problems, provided that for writing DXF a string is sufficient.
-		# FIXME before introducing new output formats.
-		
-		x = str(tokens['x'])
-		y = str(tokens['y'])
-		z = str(tokens['z'])
-			
-		if self.swapXY is True:
-			p = Point(point_id, y, x, z, text)
-		else:
-			p = Point(point_id, x, y, z, text)
-		
-		return p
+        tokens = {
+            'sequence' : line[0:7],
+            'pid' : line[8:27],
+            'text' : line[27:32],
+            'X_str' : line[36],
+            'x' : line[38:50],
+            'Y_str' : line[51],
+            'y' : line[53:66],
+            'Z_str' : line[67],
+            'z' : line[69:80]
+            }
+        
+        try:
+            int(tokens['sequence'])
+            int(tokens['pid'])
+            float(tokens['x'])
+            float(tokens['y'])
+            float(tokens['z'])
+        
+        except (ValueError, IndexError):
+            
+            is_point = False
+        
+        else:
+            
+            is_point = True
+        
+        return is_point
+    
+    def get_point(self,line):
+        
+        text = line[27:32].split()
+        
+        if len(text) >= 1 :
+            aux = text[0]
+        else:
+            aux = ""
+        
+        tokens = {
+            'pid' : line[8:27].split()[0],   # the result is more elegant than
+            'text' : aux,                    # the code (Heisenberg rocks!)
+            'x' : line[38:50].split()[0],
+            'y' : line[53:66].split()[0],
+            'z' : line[69:80].split()[0]
+            }
+                    
+        point_id = int(tokens['pid'])
+        text = str(tokens['text'])
+        
+        # note that for now we keep floats into strings to avoid approximation
+        # problems, provided that for writing DXF a string is sufficient.
+        # FIXME before introducing new output formats.
+        
+        x = str(tokens['x'])
+        y = str(tokens['y'])
+        z = str(tokens['z'])
+            
+        if self.swapXY is True:
+            p = Point(point_id, y, x, z, text)
+        else:
+            p = Point(point_id, x, y, z, text)
+        
+        return p
+
