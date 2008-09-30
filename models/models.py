0522eaeda9100a0a07e7998d2e5dbee9edf1cb50
Stefano Costa <steko@iosa.it>
2008-09-30 20:23:24 +0200

    New module to keep the list of supported models.
    Useful for user interfaces.

---
+++ models/models.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# filename: models.py
+# Copyright 2008 Luca Bianconi <luxetluc@yahoo.it>
+# Copyright 2008 Stefano Costa <steko@iosa.it>
+# Under the GNU GPL 3 License
+
+models = {
+    'Leica TCR 1205' : 'leica_tcr_1205',
+    'Zeiss Elta R55' : 'zeiss_elta_r55'
+    }
+
+if __name__ == '__main__':
+    print("List of supported models:\n------------------------")
+    for k in models.keys():
+        print k
+    print("")
+
