91c61d8190bef07b5999856eb2216ce11a9f4851
Luca Bianconi <lc.bianconi@gmail.com>
2008-09-30 21:19:47 +0200

    Added Tops' command-line interface

---
+++ models/zeiss_elta_r55.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# filename: zeiss_elta_r55.py
+
+from generic import *
+
+class ZeissConn(Connector):
+    pass
+
+class ZeissEltaR55(TotalStation):
+    
+    def __init__(self, filename):
+        TotalStation.__init__(self, filename, swapXY=True)
+    
+    def is_point(self,line):
+
+        tokens = {
+            'sequence' : line[0:7],
+            'pid' : line[8:27],
+            'text' : line[27:32],
+            'X_str' : line[36],
+            'x' : line[38:50],
+            'Y_str' : line[51],
+            'y' : line[53:66],
+            'Z_str' : line[67],
+            'z' : line[69:80]
+            }
+        
+        try:
+            int(tokens['sequence'])
+            int(tokens['pid'])
+            float(tokens['x'])
+            float(tokens['y'])
+            float(tokens['z'])
+        
+        except (ValueError, IndexError):
+            
+            is_point = False
+        
+        else:
+            
+            is_point = True
+        
+        return is_point
+    
+    def get_point(self,line):
+        '''Gets a point from a line retrieving basic data.'''
+        
+        tokens = {
+            'pid' : line[8:27].strip(),   # the result is more elegant than
+            'text' : line[27:32].strip(), # the code (Heisenberg rocks!)
+            'x' : line[38:50].strip(),
+            'y' : line[53:66].strip(),
+            'z' : line[69:80].strip()
+            }
+                    
+        point_id = int(tokens['pid'])
+        text = str(tokens['text'])
+        
+        # note that for now we keep floats into strings to avoid approximation
+        # problems, provided that for writing DXF a string is sufficient.
+        # FIXME before introducing new output formats.
+        # We could use string formatting operations to store data as floats
+        # and convert them to strings with the needed precision on the fly.
+        
+        x = str(tokens['x'])
+        y = str(tokens['y'])
+        z = str(tokens['z'])
+            
+        if self.swapXY is True:
+            p = Point(point_id, y, x, z, text)
+        else:
+            p = Point(point_id, x, y, z, text)
+        
+        return p
+
---
+++ output/dat/tops_dat.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# filename: tops_dat.py
+# Copyright 2008 Stefano Costa <steko@iosa.it>
+# Under the GNU GPL 3 License
+
+
+def to_dat(e):
+    string = "%s %s %s %s\n" % (e[0], e[0], e[1], e[2])
+    return string
+
+
+class TotalOpenDAT:
+    
+    """
+    Exports points data in DAT format suitable for use with Archis.
+    
+    ``data`` should be an iterable (e.g. list) containing one iterable (e.g.
+    tuple) for each point. The default order is PID, x, x, z, TEXT.
+    
+    This is consistent with our current standard.
+    """
+    
+    def __init__(self,data,filepath):
+        output = open(filepath, "wb")
+        lines = [ to_dat(e) for e in data ]
+        output.writelines(lines)
+        output.close()
+
+
+if __name__ == "__main__":
+    TotalOpenDAT(
+        [
+            (1,2,3,4,'qwerty'),
+            ("2.3",42,45,12,'asdfg')
+        ],
+    'tops.dat')
+
