f383d79edb3e40343e6e177e16202e9fcb2e08a5
Luca Bianconi <lc.bianconi@gmail.com>
2008-10-18 14:10:40 +0200

    Added Simple Graph preview

---
+++ tops_serial_IO.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# filename: interactive.py
+# Copyright 2008 Stefano Costa <steko@iosa.it>
+# Under the GNU GPL 3 License
+
+from models import *
+
+
+station = zeiss_elta_r55.ModelConnector('/dev/ttyUSB0')
+station.open_conn()
+
+# start from the device...
+a = raw_input("Press ENTER when download has finished\n")
+# wait until finished!
+
+result = station.download()
+print result
+
+e = open('interactive_download.txt', 'w')
+e.write(result)
+e.close()
+
