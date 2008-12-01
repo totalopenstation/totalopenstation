0dc4883fd66f5c2ad66dec492d0be15e17f14a44
Stefano Costa <steko@iosa.it>
2008-12-01 15:13:48 +0100

    Add a new module with list of output formats.
    Same idea as the models' one. Useful for GUI and mapping of
    strings to module names.

---
+++ output/formats.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# filename: output/formats.py
+# Copyright 2008 Luca Bianconi <luxetluc@yahoo.it>
+# Copyright 2008 Stefano Costa <steko@iosa.it>
+# Under the GNU GPL 3 License
+
+formats = {
+    'DXF' : 'tops_dxf',
+    'CSV' : 'tops_csv',
+    'SQL' : 'tops_sql',
+    'DAT' : 'tops_dat'
+    }
+
+if __name__ == '__main__':
+    print("List of supported output formats:\n------------------------")
+    for k in models.keys():
+        print k
+    print("")
+
