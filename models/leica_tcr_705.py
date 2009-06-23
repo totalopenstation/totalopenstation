4be3dc3cd7f87ba09a1b89bac4bcdc8ff4c2f968
Stefano Costa <steko@iosa.it>
2009-06-23 18:45:40 +0200

    Fix Trimble parser

--- models/leica_tcr_705.py
+++ models/leica_tcr_705.py
@@ -14,41 +14,38 @@ class ModelConnector(Connector):
 
 
 class ModelParser(Parser):
-    
-    def is_point(self,line):
-        
-        tokens = line.split(",")
-        is_point = True
-	
-        try:
 
+    def is_point(self,line):
+
+        tokens = line.split(",")
+        is_point = False
+
+        try:
             float(tokens[1])
             float(tokens[2])
             float(tokens[3])
-	    
-	    
         except (ValueError, IndexError):
             is_point = False
-	    
-        #else:
-            #di questo controllo che segue FORSE non gliene frega un beliscimu
-            #if tokens[4]=="MEAS":
-            #    is_point = True
-            #else:
-            #    is_point = False
-        
+        else:
+            is_point = True
+            x, y, z = tokens[1:4]
+
+        # not so clear why there are such points recorded
+        if x == '1.00' and y == '1.00' and z == '1.00':
+            is_point = False
+
         return is_point
-        
+
     def get_point(self,line):
-        
+
         tokens = line.split(",")
-        
+
         if len(tokens)> 4:
             text = str(tokens[4])
         else:
             text = ""
-        
+
         p = Point(str(tokens[0]), float(tokens[1]), float(tokens[2]), float(tokens[3]), text)
-	
+
         return p
 
