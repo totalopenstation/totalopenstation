93b83e27907ef770a34623fb165ec6838d7429d7
Alessandro Bezzi <alessandro.bezzi@arc-team.com>
2009-06-29 22:51:25 +0200

    Retrieve point is and annotated text from Trimble.
    Trimble basic support should now be complete.

--- models/trimble.py
+++ models/trimble.py
@@ -3,6 +3,7 @@
 # filename: trimble.py
 # Copyright 2009 Luca Bianconi <luxetluc@yahoo.it>
 # Copyright 2009 Stefano Costa <steko@iosa.it>
+# Copyright 2009 Alessandro Bezzi <alessandro.bezzi@arc-team.com>
 # Under the GNU GPL 3 License
 
 from generic import *
@@ -14,20 +15,24 @@ class ModelConnector(Connector):
 
 
 class ModelParser(Parser):
-    
+
     def is_point(self,line):
-        
+
         is_point = False
 
-        if "37=" in line and "38=" in line and "39=" in line:
+        if "5=" and "4=" and "37=" and "38=" and "39=" in line:
             is_point = True
-    
+
         return is_point
 
     def get_point(self,chunk):
         tokens = {}
         lines = chunk.splitlines()
         for i in lines:
+            if i.startswith('5='):
+                tokens['n'] = i.split('=')[1]
+            if i.startswith('4='):
+                tokens['p'] = i.split('=')[1]
             if i.startswith('37='):
                 tokens['x'] = i.split('=')[1]
             if i.startswith('38='):
@@ -35,14 +40,21 @@ class ModelParser(Parser):
             if i.startswith('39='):
                 tokens['z'] = i.split('=')[1]
         tokens['text'] = lines[0]
+
         try:
-            p = Point('', tokens['x'], tokens['y'], tokens['z'], '')
+            p = Point(
+                tokens['n'],
+                tokens['x'],
+                tokens['y'],
+                tokens['z'],
+                tokens['p']
+                )
         except KeyError:
             pass
         else:
             return p
 
     def split_points(self):
-        splitted_points = self.data.split('5=')
+        splitted_points = self.data.split('0=')
         return splitted_points
 
