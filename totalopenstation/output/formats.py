bf367d4ce2b382609f91a0985d9641f1ea70cd06
Stefano Costa <steko@iosa.it>
2010-02-20 16:02:38 +0100

    Fix a bug in adding optional formats

--- totalopenstation/output/formats.py
+++ totalopenstation/output/formats.py
@@ -2,8 +2,23 @@
 # -*- coding: utf-8 -*-
 # filename: output/formats.py
 # Copyright 2008 Luca Bianconi <luxetluc@yahoo.it>
-# Copyright 2008-2009 Stefano Costa <steko@iosa.it>
-# Under the GNU GPL 3 License
+# Copyright 2008-2010 Stefano Costa <steko@iosa.it>
+#
+# This file is part of Total Open Station.
+#
+# Total Open Station is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Total Open Station is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Total Open Station.  If not, see
+# <http://www.gnu.org/licenses/>.
 
 formats = {
     'DXF': 'tops_dxf',
@@ -19,7 +34,7 @@ try:
 except ImportError:
     pass
 else:
-    formats['GeoJSON': 'tops_geojson']
+    formats['GeoJSON'] = 'tops_geojson'
 
 if __name__ == '__main__':
     print("List of supported output formats:\n------------------------")
