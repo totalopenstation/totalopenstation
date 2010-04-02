14b5c2fba6e8a0f869edb9f80a685ff66b2fa467
Luca Bianconi <lc.bianconi@gmail.com>
2010-04-03 00:02:48 +0200

    #6 Added user preferences saving with licence

--- totalopenstation/utils/upref.py
+++ totalopenstation/utils/upref.py
@@ -1,5 +1,24 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# filename: totalopenstation-gui.py
+# Copyright 2008-2010 Stefano Costa <steko@iosa.it>
+# Copyright 2010 Luca Bianconi <luxetluc@yahoo.it>
+#
+# This file is part of Total Open Station.
+#
+# Total Open Station is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Total Open Station is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Total Open Station.  If not, see
+# <http://www.gnu.org/licenses/>.
 
 import atexit
 import os
