2f3d48ff7dbab6e48879184c6a3233708caa7009
Stefano Costa <steko@iosa.it>
2011-06-20 14:46:24 +0200

    [output] fix #13 using cStringIO instead of StringIO.
    tops_csv was actually the only module using StringIO, because we're using
    the csv.Writer class that needs a File-like object.

--- totalopenstation/output/tops_csv.py
+++ totalopenstation/output/tops_csv.py
@@ -1,11 +1,26 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 # filename: tops_csv.py
-# Copyright 2008-2009 Stefano Costa <steko@iosa.it>
-# Under the GNU GPL 3 License
+# Copyright 2008, 2009, 2011 Stefano Costa <steko@iosa.it>
+#
+# This file is part of Total Open Station.
+#
+# Total Open Station is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Total Open Station is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Total Open Station.  If not, see
+# <http://www.gnu.org/licenses/>.
 
 import csv
-import StringIO
+import cStringIO
 
 
 class OutputFormat:
@@ -22,7 +37,7 @@ class OutputFormat:
 
     def __init__(self, data):
         self.data = data
-        self.output = StringIO.StringIO()
+        self.output = cStringIO.StringIO()
         self.writer = csv.writer(self.output, quoting=csv.QUOTE_NONNUMERIC)
 
     def process(self):
