eec0996c7346f4e6b155057cc5541db773407c82
Damien Gaignon <damien.gaignon@gmail.com>
2016-03-08 12:45:22 +0100

    Change CSV output to add more information Fix CSV output unittest

--- totalopenstation/tests/test_csv.py
+++ totalopenstation/tests/test_csv.py
@@ -8,13 +8,15 @@ class TestCSVOutput(unittest.TestCase):
     def setUp(self):
         self.data = [
             Feature(Point(12.8, 76.3, 56.2),
-                    desc='TEST POINT',
+                    desc='PT',
+                    point_name='TEST POINT',
                     id=1),
             Feature(Point(19.8, 26.3, 46.2),
-                    desc='TEST POINT #2',
+                    desc='PT',
+                    point_name='TEST POINT #2',
                     id=2),
         ]
 
     def test_output(self):
         self.output = OutputFormat(self.data).process()
-        self.assertEqual(self.output.splitlines()[1], '1,12.8,76.3,56.2,"TEST POINT"')
+        self.assertEqual(self.output.splitlines()[1], '1,"PT","TEST POINT",12.8,76.3,56.2,"","","","","",""')
