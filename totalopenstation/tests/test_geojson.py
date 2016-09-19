4097c96a1f966f23acc43fbc8d40a868d4666ea5
Stefano Costa <steko@iosa.it>
2016-09-19 21:14:34 +0200

    As already noted, internal data is an iterable of Feature objects

--- totalopenstation/tests/test_geojson.py
+++ totalopenstation/tests/test_geojson.py
@@ -7,14 +7,14 @@ from totalopenstation.output.tops_geojson import OutputFormat
 class TestGeoJSONOutput(unittest.TestCase):
 
     def setUp(self):
-        self.data = FeatureCollection([
+        self.data = [
             Feature(Point(12.8, 76.3, 56.2),
                     desc='TEST POINT',
                     id=1),
             Feature(Point(19.8, 26.3, 46.2),
                     desc='TEST POINT #2',
                     id=2),
-        ])
+        ]
 
     def test_output(self):
         self.output = OutputFormat(self.data).process()
