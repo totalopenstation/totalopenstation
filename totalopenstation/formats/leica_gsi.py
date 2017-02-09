257f35a62df326cb38d3238d40cf558d637b91d3
Damien Gaignon <damien.gaignon@gmail.com>
2017-02-09 15:53:35 +0100

    Doc : Add docstring to Leica GSI parser for documentation
    Docstrings are added to the Leica GSI parser module for classes, methods
    and attributes.
    Docstrings are also added for privates methods even if they are not
    included in final documentation.

--- totalopenstation/formats/leica_gsi.py
+++ totalopenstation/formats/leica_gsi.py
@@ -35,11 +35,14 @@ UNITS = {"angle": {'21', '22', '25'},
 
 
 class FormatParser(Parser):
-    '''A FormatParser for Leica GSI data format.
+    '''The FormatParser for Leica GSI data format.
 
-    It doesn't inherit from the base Parser class because the internal
-    procedure is quite different, but it implements the same API so it
-    can work nicely with other parts of the library.'''
+    Args:
+        data (str): A string representing the file to be parsed.
+            
+    Attributes:
+        line (list): A list of each lines of the file being parsed.
+    '''
 
     def __init__(self, data):
         self.line = data.splitlines()
@@ -47,6 +50,9 @@ class FormatParser(Parser):
     def _get_comments(self):
         """
         Get all comments of the parsed line
+        
+        Returns:
+            A list of comments or an empty list
         """
         comments = []
         for i in range(1, 10):
@@ -60,6 +66,9 @@ class FormatParser(Parser):
     def _get_attrib(self):
         """
         Get all attributes or remarks of the parsed line
+        
+        Returns:
+            A list of attributes and remarks or an empty list
         """
         attrib = []
         for i in range(1, 10):
@@ -73,6 +82,15 @@ class FormatParser(Parser):
     def _get_coordinates(self, first_coor, unit):
         """
         Get all coordinates of the parsed line
+        
+        Args:
+            first_coor (str): The Word Index of the first coordinate.
+                Could be 81 or 84
+            unit (int): The divider corresponding to the coordinate unit.
+
+        Returns:
+            All three coordinates in the forme of X, Y, Z
+            If these coordinates do not exist, return None
         """
         second_coor = str(int(first_coor) + 1)
         third_coor = str(int(first_coor) + 2)
@@ -94,6 +112,11 @@ class FormatParser(Parser):
     def _get_angle(self, angle, unit):
         """
         Get an angle of the parsed line
+        
+        Returns:
+            A floating number representing the angle
+            If this angle does not exist, return None
+            
         """
         try:
             angle_sign, angle_data = self.tdict[angle]['sign'], self.tdict[angle]['data']
@@ -107,6 +130,10 @@ class FormatParser(Parser):
     def _get_edm_accuracy(self, ldata):
         """
         Get the ppm and the prism constant of the parsed line
+        
+        Returns:
+            Two floating numbers representing ppm and prism constant
+            If these values do not exist, return None
         """
         try:
             ppm_sign, ppm_data = self.tdict['51']['sign'], self.tdict['51']['data'][:ldata-4]
@@ -130,6 +157,9 @@ class FormatParser(Parser):
     def _get_value(self, value, unit):
         """
         Get a value of the parsed line
+        
+        Returns:
+            A string value or None
         """
         try:
             value_sign, value_data = self.tdict[value]['sign'], self.tdict[value]['data']
@@ -141,6 +171,25 @@ class FormatParser(Parser):
         return value
 
     def _points(self):
+        '''Extract all GSI data.
+
+        This parser is based on the information in :ref:`if_leica_gsi`
+        
+        Returns:
+            A list of GeoJSON-like Feature object representing points coordinates.
+    
+        Raises:
+            KeyError: An error occured during line read, this line could not be 
+                computed as the WI does not exist.
+            KeyError: An error occured during computation, the data does not exist.
+        
+        Notes:
+            Information needed are:
+                - station : 11, 84, 85, 86, 88
+                - direct point : 11, 81, 82, 83
+                - computed point : 11, 21, 22, 31 or 32, 87 [, 88] [, 81, 82, 83]
+        '''
+        
         points = []
         bp = None
         ldata = len(self.line[0].split()[0].lstrip('*')[7:])
@@ -257,21 +306,22 @@ class FormatParser(Parser):
     def raw_line(self):
         '''Extract all GSI data.
 
-        Based on the "GSI ONLINE for Leica TPS" document.
-
-        Information needed are:
-            - station : 11 [, 25], 84, 85, 86 [, 87], 88
-            - direct point : 11, 81, 82, 83
-            - computed point : 11, 21, 22, 31 or 32 [, 51], 87 [, 88] [, 81, 82, 83]
-
-        Units are unchanged, format of the original parsed file
-
-        TODO:
-            - get coordinates order (NEZ or ENZ)
-            - add all missing code
-            - get comments
-            - add an option to link the comment to either previous or next line
-            - add the possibility to customize code
+        This parser is based on the information in :ref:`if_leica_gsi`
+        
+        Returns:
+            A list of GeoJSON-like Feature object representing raw data
+                i.e. polar coordinates and other informations.
+    
+        Raises:
+            KeyError: An error occured during line read, this line could not be 
+                computed as the WI does not exist.
+            KeyError: An error occured during computation, the data does not exist.
+        
+        Notes:
+            Information needed are:
+                - station : 11 [, 25], 84, 85, 86 [, 87], 88
+                - direct point : 11, 81, 82, 83
+                - computed point : 11, 21, 22, 31 or 32 [, 51], 87 [, 88] [, 81, 82, 83]
         '''
 
         points = []
