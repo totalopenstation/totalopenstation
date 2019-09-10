ce7bcae38f6b7bf8111bd71d1a2d9a65c22a4ba9
Stefano Costa <steko@iosa.it>
2019-09-10 16:06:27 +0200

    Convert CSV output module to DictWriter.
    This reduces complexity in the source code.

--- totalopenstation/output/tops_csv.py
+++ totalopenstation/output/tops_csv.py
@@ -35,65 +35,71 @@ class OutputFormat:
     def __init__(self, data):
         self.data = data
         self.output = io.StringIO()
-        self.writer = csv.writer(self.output, quoting=csv.QUOTE_NONNUMERIC)
+        fieldnames = ['pid', 'type', 'point_name', 'x', 'y', 'z', 'angle', 'z_angle', 'distance',
+                      'th', 'ih', 'circle', 'station']
+        self.writer = csv.DictWriter(self.output, quoting=csv.QUOTE_NONNUMERIC, fieldnames=fieldnames)
+        self.writer.writeheader()
 
     def process(self):
-        try:
-            self.data[0].geometry.z
-        except ValueError:
-            self.writer.writerow(('PID', 'type', 'Point Name', 'x', 'y', 'angle', 'z_angle', 'distance',
-                                 'th', 'ih', 'circle', 'station'))
-        else:
-            self.writer.writerow(('PID', 'type', 'Point Name', 'x', 'y', 'z', 'angle', 'z_angle', 'distance',
-                                  'th', 'ih', 'circle', 'station'))
         for feature in self.data:
-            row = [feature.id,
-                   feature.desc]
-            try:  # not all input formats include point_name
-                row.append(feature.point_name)
-            except KeyError:
-                row.append('')
-            row.extend([feature.geometry.x,
-                        feature.geometry.y])
-            try:
-                row.append(feature.geometry.z)
-            except ValueError:
-                pass
-            if feature.desc == "PT":
-                row.extend([""] * 6)
-            if feature.desc == "ST":
-                row.extend([""] * 4)
-                try:
-                    row.extend([feature.properties["ih"],
-                               ""])
-                except KeyError:
-                    row.extend([""] * 2)
-            if feature.desc == "BS":
-                row.extend([""] * 5)
-                row.extend([feature.properties["circle"]])
-            if feature.desc == "PO":
-                try:
-                    feature.properties["azimuth"]
-                except KeyError:
-                    pass
-                else:
-                    angle = feature.properties["azimuth"]
-                if feature.properties["angle"] is not None:
-                    angle = feature.properties["angle"]
+            row = {
+                'pid': feature.id,
+                'type': feature.desc,
+                'x' : feature.geometry.x,
+                'y': feature.geometry.y
+            }
 
-                if feature.properties["slope_dist"] is not None:
-                    dist = feature.properties["slope_dist"]
-                elif feature.properties["horizontal_dist"] is not None:
-                    dist = feature.properties["horizontal_dist"]
-                row.extend([angle,
-                           feature.properties["z_angle"],
-                           dist,
-                           feature.properties["th"]])
-                row.extend([""] * 2)
+            try:  # not all input formats include z coordinates
+                row['z'] = feature.geometry.z
+            except ValueError:
+                row['z'] = ''
+
+            try:  # not all input formats include point_name
+                row['point_name'] = feature.point_name
+            except KeyError:
+                row['point_name'] = ''
+
+            try:  # not all input formats include instrument height
+                row['ih'] = feature.properties['ih']
+            except KeyError:
+                row['ih'] = ''
+
+            try:  # not all input formats include circle FIXME what is circle?
+                row['circle'] = feature.properties['circle']
+            except KeyError:
+                row['circle'] = ''
+
+            try:  # not all input formats include azimuth/angle
+                row['angle'] = feature.properties['azimuth']
+            except KeyError:
                 try:
-                    row.extend([feature.properties["st_name"]])
+                    row['angle'] = feature.properties['angle']
                 except KeyError:
-                    row.extend([""])
+                    row['angle'] = ''
+
+            try:  # not all input formats include distance
+                row['dist'] = feature.properties['slope_dist']
+            except KeyError:
+                try:
+                    row['distance'] = feature.properties['horizontal_dist']
+                except KeyError:
+                    row['distance'] = ''
+
+            try:  # not all input formats include z_angle
+                row['z_angle'] = feature.properties['z_angle']
+            except KeyError:
+                row['z_angle'] = ''
+
+            try:  # not all input formats include target height
+                row['th'] = feature.properties['th']
+            except KeyError:
+                row['th'] = ''
+
+            try:  # not all input formats include station name
+                row['station'] = feature.properties['st_name']
+            except KeyError:
+                row['station'] = ''
+
             self.writer.writerow(row)
 
         return self.output.getvalue()
