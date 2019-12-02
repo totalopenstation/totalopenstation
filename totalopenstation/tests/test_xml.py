a44b6bf11aa5a2d2f8a1e63eb37c62432e60e30f
Damien Gaignon <damien.gaignon@gmail.com>
2019-12-02 14:46:18 +0100

    Parser : Add unittest to LandXML output
    This commit add a simple unittest to LandXML output.

---
+++ totalopenstation/tests/test_xml.py
@@ -0,0 +1,23 @@
+import unittest
+
+from totalopenstation.formats import Feature, Point
+from totalopenstation.output.tops_xml import OutputFormat
+
+class TestXMLOutput(unittest.TestCase):
+
+    def setUp(self):
+        self.data = [
+            Feature(Point(12.8, 76.3, 56.2),
+                    desc='PT',
+                    point_name='TEST POINT',
+                    id=1),
+            Feature(Point(19.8, 26.3, 46.2),
+                    desc='PT',
+                    point_name='TEST POINT #2',
+                    id=2),
+        ]
+
+    def test_output(self):
+        self.output = OutputFormat(self.data).process()
+        self.assertEqual(self.output.splitlines()[9], '\t\t\t<CgPoint name="TEST POINT">12.8 76.3 56.2</CgPoint>')
+        self.assertEqual(self.output.splitlines()[10], '\t\t\t<CgPoint name="TEST POINT #2">19.8 26.3 46.2</CgPoint>')
