ad2eb35b416146835dd824e6a8439cb240c089d6
enzo <enzo.ccc@gmail.com>
2021-09-24 16:50:55 +0200

    Add files via upload

--- totalopenstation/tests/test_sokkia_sdr33_polar.py
+++ totalopenstation/tests/test_sokkia_sdr33_polar.py
@@ -20,7 +20,7 @@ class TestSokkiaParser(unittest.TestCase):
         self.assertAlmostEqual(self.fp.points[0].geometry.coords[0][2], 0.000)
 
     def test_feature(self):
-        self.assertEqual(self.fp.points[0].id, 'STZTEMP')
+        self.assertEqual(self.fp.points[0].id, ' STZTEMP')
         self.assertEqual(self.fp.points[0].desc, 'STZTEMP')
         self.assertEqual(self.fp.points[1].desc, 'STZTEMP')
 
