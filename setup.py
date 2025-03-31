b013af2c16e8a75ebd0ea40898a256ef0d7c6fe1
Stefano Costa <steko@iosa.it>
2025-03-31 22:34:03 +0200

    chore: align Python versions to check in tox, pytest and setup.py

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Topic :: Scientific/Engineering :: GIS',
         ],
     keywords='survey, geodimeter, fieldwork, format conversion',
